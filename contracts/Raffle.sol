d77c5a6fb6bfd99bba81d57b80cc631ec2b2dd19
Vahid Abolhassani <vahid.abolhassani2@gmail.com>
2025-09-16 16:51:41 +0330

    make Raffle.sol

---
+++ contracts/Raffle.sol
@@ -0,0 +1,11 @@
+//Enter the lotteryy (paying some amount)
+//Pick a random winner (verifiably random)
+//Winner to be selected every X minutes -> completely automated
+//Chainlink Oracle -> Randomness, Automated execution (Chainlink Keepers)
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+contract Raffle {
+  constructor() {}
+}
